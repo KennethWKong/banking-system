78089f7e5fef8625f49ddc729992265b8dadaafa
Kenneth Kong <200035982+KennethWKong@users.noreply.github.com>
2026-01-09 20:14:24 -0500

    Create Transfer Request DTO, for structured input.

---
+++ src/main/java/com/waterkong/banking_system/dto/TransferRequest.java
@@ -0,0 +1,12 @@
+package com.waterkong.banking_system.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class TransferRequest {
+
+
+
+    
+}
