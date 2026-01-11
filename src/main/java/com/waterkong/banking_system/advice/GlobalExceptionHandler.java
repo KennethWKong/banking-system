ba92fc0dd57375bd2a0f79c11172ae8aad1da894
Kenneth Kong <200035982+KennethWKong@users.noreply.github.com>
2026-01-11 00:42:44 -0500

    Spring will return errors clearly now

--- src/main/java/com/waterkong/banking_system/advice/GlobalExceptionHandler.java
+++ src/main/java/com/waterkong/banking_system/advice/GlobalExceptionHandler.java
@@ -12,4 +12,17 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleAccountNotFound(AccountNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(NotEnoughFundsException.class)
+    public ResponseEntity<Map<String, String>> handleNotEnoughFunds(NotEnoughFundsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", ex.getMessage()));
+    }
 }
