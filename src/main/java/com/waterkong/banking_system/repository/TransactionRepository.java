8bf6e7588f880717b8bdb85a039b90472d2cda93
Kenneth Kong <200035982+KennethWKong@users.noreply.github.com>
2026-01-11 19:10:07 -0500

    Spring Data reads method name and builds SQL

---
+++ src/main/java/com/waterkong/banking_system/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.waterkong.banking_system.repository;
+
+import com.waterkong.banking_system.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountIdOrderByCreatedAtDesc(Long accountId);
+}
